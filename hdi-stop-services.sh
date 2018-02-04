46d4d8104312717944afa4dec48371baa8e77341
Igor Izotov <iizotov@users.noreply.github.com>
2018-02-05 10:59:08 +1100

    Script Action for Azure HDInsight to stop unnecessary services
    Usage: sudo -E bash hdi-stop-services.sh <service0-to-stop> [<service1-to-stop>] [<service2-to-stop>] ... [<serviceN-to-stop>]
    
    can be run manually or added as a Script Action to run during cluster creation: https://docs.microsoft.com/en-us/azure/hdinsight/hdinsight-hadoop-customize-cluster-linux#use-a-script-action-during-cluster-creation

---
+++ hdi-stop-services.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+usage() {
+    echo ""
+    echo "Usage: sudo -E bash $0 <service0-to-stop> [<service1-to-stop>] [<service2-to-stop>] ... [<serviceN-to-stop>]" ;
+    echo "This script does NOT require Ambari username and password";
+    exit 132;
+}
+
+#validate user input
+if [ -z "$1" ]
+    then
+        usage
+        echo "Error: at least one service should be provided."
+        exit 137
+fi
+
+AMBARICONFIGS_SH=/var/lib/ambari-server/resources/scripts/configs.sh
+PORT=8080
+
+ACTIVEAMBARIHOST=headnodehost
+
+#Import helper module
+wget -O /tmp/HDInsightUtilities-v01.sh -q https://hdiconfigactions.blob.core.windows.net/linuxconfigactionmodulev01/HDInsightUtilities-v01.sh && source /tmp/HDInsightUtilities-v01.sh && rm -f /tmp/HDInsightUtilities-v01.sh
+
+checkHostNameAndSetClusterName() {
+	PRIMARYHEADNODE=`get_primary_headnode`
+    
+	#Check if values retrieved are empty, if yes, exit with error
+	if [[ -z $PRIMARYHEADNODE ]]; then
+	echo "Could not determine primary headnode."
+	exit 139
+	fi
+
+	fullHostName=$(hostname -f)
+    echo "fullHostName=$fullHostName. Lower case: ${fullHostName,,}"
+    echo "primary headnode=$PRIMARYHEADNODE. Lower case: ${PRIMARYHEADNODE,,}"
+    if [ "${fullHostName,,}" != "${PRIMARYHEADNODE,,}" ]; then
+        echo "$fullHostName is not primary headnode. This script has to be run on $PRIMARYHEADNODE."
+        exit 0
+    fi
+    CLUSTERNAME=$(sed -n -e 's/.*\.\(.*\)-ssh.*/\1/p' <<< $fullHostName)
+    if [ -z "$CLUSTERNAME" ]; then
+        CLUSTERNAME=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nprint ClusterManifestParser.parse_local_manifest().deployment.cluster_name" | python)
+        if [ $? -ne 0 ]; then
+            echo "[ERROR] Cannot determine cluster name. Exiting!"
+            exit 133
+        fi
+    fi
+    echo "Cluster Name=$CLUSTERNAME"
+}
+
+checkHostNameAndSetClusterName
+
+validateUsernameAndPassword() {
+    coreSiteContent=$(bash $AMBARICONFIGS_SH -u $USERID -p $PASSWD get $ACTIVEAMBARIHOST $CLUSTERNAME core-site)
+    if [[ $coreSiteContent == *"[ERROR]"* && $coreSiteContent == *"Bad credentials"* ]]; then
+        echo "[ERROR] Username and password are invalid. Exiting!"
+        exit 134
+    fi
+}
+
+stopServiceViaRest() {
+    if [ -z "$1" ]; then
+        echo "Need service name to stop service"
+        exit 136
+    fi
+    SERVICENAME=$1
+    echo "Stopping $SERVICENAME"
+    curl -u $USERID:$PASSWD -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop Service for adding storage account"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://$ACTIVEAMBARIHOST:$PORT/api/v1/clusters/$CLUSTERNAME/services/$SERVICENAME
+}
+
+startServiceViaRest() {
+    if [ -z "$1" ]; then
+        echo "Need service name to start service"
+        exit 136
+    fi
+    sleep 2
+    SERVICENAME=$1
+    echo "Starting $SERVICENAME"
+    startResult=$(curl -u $USERID:$PASSWD -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Service after adding storage account"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$ACTIVEAMBARIHOST:$PORT/api/v1/clusters/$CLUSTERNAME/services/$SERVICENAME)
+    if [[ $startResult == *"500 Server Error"* || $startResult == *"internal system exception occurred"* ]]; then
+        sleep 60
+        echo "Retry starting $SERVICENAME"
+        startResult=$(curl -u $USERID:$PASSWD -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Service after adding storage account"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://$ACTIVEAMBARIHOST:$PORT/api/v1/clusters/$CLUSTERNAME/services/$SERVICENAME)
+    fi
+    echo $startResult
+}
+
+##############################
+if [ "$(id -u)" != "0" ]; then
+    echo "[ERROR] The script has to be run as root."
+    usage
+fi
+
+USERID=$(echo -e "import hdinsight_common.Constants as Constants\nprint Constants.AMBARI_WATCHDOG_USERNAME" | python)
+
+echo "USERID=$USERID"
+
+PASSWD=$(echo -e "import hdinsight_common.ClusterManifestParser as ClusterManifestParser\nimport hdinsight_common.Constants as Constants\nimport base64\nbase64pwd = ClusterManifestParser.parse_local_manifest().ambari_users.usersmap[Constants.AMBARI_WATCHDOG_USERNAME].password\nprint base64.b64decode(base64pwd)" | python)
+
+validateUsernameAndPassword
+
+for arg in "$@"
+do
+	stopServiceViaRest ${arg^^}
+    echo "Stopped ${arg^^}"
+done
+
+#sleep for a bit before exiting
+sleep 5
