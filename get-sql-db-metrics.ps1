5bf9580721d9f0ef3cb26ce8dd15c50703ce5e69
Igor Izotov <iizotov@users.noreply.github.com>
2018-02-15 11:38:58 +1100

    reformatted get-sql-db-metrics.ps1

--- get-sql-db-metrics.ps1
+++ get-sql-db-metrics.ps1
@@ -23,26 +23,26 @@
  .PARAMETER outputFolder
     Folder to output to (will be created if doesn't exist),
 #>
-[CmdletBinding(DefaultParameterSetName="ScanSelectedSubscriptions")]            
+[CmdletBinding(DefaultParameterSetName = "ScanSelectedSubscriptions")]            
 
 param(
- [Parameter(ParameterSetName='scanAll', Mandatory=$True)]
- [switch]$scanAllSubscriptions,
+    [Parameter(ParameterSetName = 'scanAll', Mandatory = $True)]
+    [switch]$scanAllSubscriptions,
 
- [Parameter(ParameterSetName='ScanSelectedSubscriptions', Mandatory=$True)]
- [string[]]$subscriptionIds,
+    [Parameter(ParameterSetName = 'ScanSelectedSubscriptions', Mandatory = $True)]
+    [string[]]$subscriptionIds,
 
- [Parameter(Mandatory=$False)]
- [System.DateTime]$startDateTimeUTC = (Get-Date).ToUniversalTime().AddDays(-14),
+    [Parameter(Mandatory = $False)]
+    [System.DateTime]$startDateTimeUTC = (Get-Date).ToUniversalTime().AddDays(-14),
  
- [Parameter(Mandatory=$False)]
- [System.TimeSpan]$timeGrain = "00:05:00",
+    [Parameter(Mandatory = $False)]
+    [System.TimeSpan]$timeGrain = "00:05:00",
 
- [Parameter(Mandatory=$False)]
- [string[]]$metrics = @("storage","dtu_used"),
+    [Parameter(Mandatory = $False)]
+    [string[]]$metrics = @("storage", "dtu_used"),
 
- [Parameter(Mandatory=$False)]
- [string]$outputFolder = ".\output"
+    [Parameter(Mandatory = $False)]
+    [string]$outputFolder = ".\output"
 )
 
 
@@ -56,8 +56,7 @@ $outputFolder = (Resolve-Path $outputFolder).Path;
 
 # sign in
 Write-Host "Logging in...";
-try
-{
+try {
     $availableSubscriptionIds = (Get-AzureRmSubscription | Where-Object {$_.State -ieq 'Enabled' } ).Id;
 }
 catch {
@@ -67,32 +66,31 @@ catch {
 
 
 #get available subs
-if($scanAllSubscriptions) {
+if ($scanAllSubscriptions) {
     $subscriptionIds = $availableSubscriptionIds;  
 }
 else {
 
 }
 
-foreach($subscriptionId in $subscriptionIds) {
+foreach ($subscriptionId in $subscriptionIds) {
     #cleaning up files
     $outputPath = "$($outputFolder)/output-$($subscriptionId).csv";
     Remove-Item -Path $outputPath -ErrorAction SilentlyContinue;
     
     Write-Host "Selecting subscription $subscriptionId";
     Select-AzureRmSubscription -Subscription $subscriptionId -ErrorAction Continue | Out-Null
-    foreach($sqlServer in Get-AzureRmSqlServer) {
-		foreach($sqlDatabase in Get-AzureRmSqlDatabase -ServerName $sqlServer.ServerName -ResourceGroupName $sqlServer.ResourceGroupName) {
-			if($sqlDatabase.DatabaseName -ine "master") {
-				#"SQL databases," + $sqlDatabase.Name + ",Online"  | out-file $OutFilePath -encoding ascii -append
+    foreach ($sqlServer in Get-AzureRmSqlServer) {
+        foreach ($sqlDatabase in Get-AzureRmSqlDatabase -ServerName $sqlServer.ServerName -ResourceGroupName $sqlServer.ResourceGroupName) {
+            if ($sqlDatabase.DatabaseName -ine "master") {
                 Write-Host "Found $($sqlDatabase.DatabaseName) on $($sqlDatabase.ServerName)";
-			    foreach($metric in Get-AzureRmMetric -ResourceId $sqlDatabase.ResourceId -TimeGrain $timeGrain -StartTime $startDateTimeUTC -AggregationType Average -MetricNames $metrics -WarningAction SilentlyContinue -ErrorAction Continue){
-                    $metric.data | select TimeStamp,Average, @{l="Metric";e={$metric.Name.Value}}, @{l="Server";e={$sqlDatabase.ServerName}},@{l="Database";e={$sqlDatabase.DatabaseName}},@{l="DbSize";e={$sqlDatabase.CurrentServiceObjectiveName}} | ConvertTo-Csv -Delimiter "," | select -skip 2 >> $outputPath;
+                foreach ($metric in Get-AzureRmMetric -ResourceId $sqlDatabase.ResourceId -TimeGrain $timeGrain -StartTime $startDateTimeUTC -AggregationType Average -MetricNames $metrics -WarningAction SilentlyContinue -ErrorAction Continue) {
+                    $metric.data | select TimeStamp, Average, @{l = "Metric"; e = {$metric.Name.Value}}, @{l = "Server"; e = {$sqlDatabase.ServerName}}, @{l = "Database"; e = {$sqlDatabase.DatabaseName}}, @{l = "DbSize"; e = {$sqlDatabase.CurrentServiceObjectiveName}} | ConvertTo-Csv -Delimiter "," | select -skip 2 >> $outputPath;
                     Write-Host "finished exporting $($metric.Name.Value) for $($sqlDatabase.DatabaseName) on $($sqlDatabase.ServerName) to $outputPath";
                 }
             }
-		}
-	} 
+        }
+    } 
 }
 
 Write-Host "csv outputs are located in $outputFolder";
